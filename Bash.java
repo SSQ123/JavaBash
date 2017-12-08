24ac48ac2629a1586a5ec1885ed97a55cdcb2894
SSQ <dos1234567890a@gmail.com>
2017-12-08 11:31:46 -0500

    Update Bash.java
    First update in a while so decided to add main features at least some.

--- Bash.java
+++ Bash.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
-
+import java.io.*;
 public class Bash
 {
+  Path CurrentDir=/;
   public static Main(String[] args)
   {
-  
+    
+    //Declares Input Variable
+  Scanner Input= new Scanner(System.in);
+    while Exit == 0 do{
+    System.out(/n+"$:/");
+    if Input.equals("md") then
+      System.out("Where?");
+      Scanner DirectoryCreation = new Scanner(System.in);
+      
+      else if Input.equals("rd")
+        else if Input.equals("nano")
+          Nano = new Textedit.main();
+          else if Input.equals("exit")then
+            System.exit();
+      else if Input == ""
+            }
 }
 }
